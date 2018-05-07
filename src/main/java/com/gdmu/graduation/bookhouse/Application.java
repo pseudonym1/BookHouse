94a9949d624254f3cd3339fe35fe2ddf60a7f5c5
kaylisa <kaylisa.wang@hitachiconsulting.com>
2018-05-07 22:50:29 +0800

    basic project; the first import;

---
+++ src/main/java/com/gdmu/graduation/bookhouse/Application.java
@@ -0,0 +1,26 @@
+package com.gdmu.graduation.bookhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+
+/**
+ * The bookhouse application
+ * @author kaylisa.
+ */
+/*@EnableCaching ：开启缓存功能*/
+/*@EnableCaching*/
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer implements WebApplicationInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
---
+++ src/main/java/com/gdmu/graduation/bookhouse/model/user.java
@@ -0,0 +1,110 @@
+package com.gdmu.graduation.bookhouse.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * the user model
+ */
+@Entity
+@Table(name = "tb_user")
+public class user {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String power;
+
+    @Column(name = "CREATED_AT")
+    private Date createdAt;
+
+    @Column(name = "image_path")
+    private String image;
+
+    private String cost;
+
+    private String comment;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public void setPower(String power) {
+        this.power = power;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
